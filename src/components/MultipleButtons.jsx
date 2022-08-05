80721076857e0e51e20a86d9b685befea429c618
Joao <joaommattedi@gmail.com>
2022-08-05 19:49:54 -0300

    reqs 1 ao 6 completos

--- src/components/MultipleButtons.jsx
+++ src/components/MultipleButtons.jsx
@@ -16,14 +16,10 @@ export default class MultipleButtons extends Component {
   //   this.setState({ answers: [correctAns, ...incorrectAns], correct: correctAns });
   // }
   shuffleArray = (arr) => {
-    // Loop em todos os elementos
     for (let i = arr.length - 1; i > 0; i -= 1) {
-      // Escolhendo elemento aleatório
       const j = Math.floor(Math.random() * (i + 1));
-      // Reposicionando elemento
       [arr[i], arr[j]] = [arr[j], arr[i]];
     }
-    // Retornando array com aleatoriedade
     return arr;
   }
 
