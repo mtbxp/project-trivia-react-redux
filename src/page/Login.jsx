3e388d2aa6e31da50da3b0755b926fcc3169e56b
Joao <joaommattedi@gmail.com>
2022-08-05 15:55:24 -0300

    requisito 2

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 export default class Login extends Component {
@@ -13,6 +14,15 @@ export default class Login extends Component {
   validation = () => {
     const { email, name } = this.state;
     return !(email && name);
+  }
+
+  handleClick = async () => {
+    const response = await fetch('https://opentdb.com/api_token.php?command=request');
+    const data = await response.json();
+    localStorage.setItem('token', data.token);
+
+    const { history } = this.props;
+    history.push('/game');
   }
 
   handleChange = ({ target }) => {
@@ -47,6 +57,7 @@ export default class Login extends Component {
           data-testid="btn-play"
           type="button"
           disabled={ this.validation() }
+          onClick={ this.handleClick }
         >
           Play
         </button>
@@ -54,3 +65,9 @@ export default class Login extends Component {
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
