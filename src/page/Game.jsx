b3fb0fc203d9c39bff79f41376b61e4df3db3cc3
David <david.lourenco94@gmail.com>
2022-08-07 21:00:25 -0300

    add timer lint corrigido

--- src/page/Game.jsx
+++ src/page/Game.jsx
@@ -27,24 +27,29 @@ export default class Game extends Component {
     const { timer } = this.state;
     const second = 1000;
     if (timer > 0) {
-      setTimeout(() => this.setState({
-        timer: timer - 1,
-      }), second);
+      setTimeout(
+        () => this.setState({
+          timer: timer - 1,
+        }),
+        second,
+      );
     }
-  }
+  };
 
   validateToken = async () => {
     const token = localStorage.getItem('token');
     const { history } = this.props;
     const magicNumber = 3;
-    const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+    const response = await fetch(
+      `https://opentdb.com/api.php?amount=5&token=${token}`,
+    );
     const data = await response.json();
     if (data.response_code === magicNumber) {
       localStorage.removeItem('token');
       history.push('/');
     }
     this.setState({ results: data.results });
-  }
+  };
 
   handleClick = () => {
     const { currentQuestion } = this.state;
@@ -54,17 +59,24 @@ export default class Game extends Component {
     } else {
       this.setState({ currentQuestion: currentQuestion + 1 });
     }
-  }
+  };
 
   render() {
     const { results, currentQuestion, timer } = this.state;
     return (
       <div>
         <Header />
-        <h3>{ timer }</h3>
-        {results.length > 0
-          && <Question results={ results } currentQuestion={ currentQuestion } timer={ timer } />}
-        <button type="button" onClick={ this.handleClick }>NEXT</button>
+        <h3>{timer}</h3>
+        {results.length > 0 && (
+          <Question
+            results={ results }
+            currentQuestion={ currentQuestion }
+            timer={ timer }
+          />
+        )}
+        <button type="button" onClick={ this.handleClick }>
+          NEXT
+        </button>
       </div>
     );
   }
